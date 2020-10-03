231006520c2266e96edec10a396367c3cfaa2ef3
Muten-007 <55773259+Muten-007@users.noreply.github.com>
2020-10-03 18:14:23 +0800

    Add files via upload

---
+++ 桶排序.c
@@ -0,0 +1,53 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 数组长度
+#define LENGTH(array) ( (sizeof(array)) / (sizeof(array[0])) )
+
+
+void bucket_sort(int a[], int n, int max)
+{
+    int i, j;
+    int *buckets;
+
+    if (a==NULL || n<1 || max<1)
+        return ;
+
+    // 创建一个容量为max的数组buckets，并且将buckets中的所有数据都初始化为0。
+    if ((buckets=(int *)malloc(max*sizeof(int)))==NULL)
+        return ;
+    memset(buckets, 0, max*sizeof(int));
+
+    // 1. 计数
+    for(i = 0; i < n; i++) 
+        buckets[a[i]]++; 
+
+    // 2. 排序
+    for (i = 0, j = 0; i < max; i++) 
+        while( (buckets[i]--) >0 )
+            a[j++] = i;
+
+    free(buckets);
+}
+
+void main()
+{
+    int i;
+    int a[] = {8,2,3,4,3,6,6,3,9};
+    int ilen = LENGTH(a);
+
+    printf("before sort:");
+    for (i=0; i<ilen; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+
+    bucket_sort(a, ilen, 10); // 桶排序
+
+    printf("after  sort:");
+    for (i=0; i<ilen; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
